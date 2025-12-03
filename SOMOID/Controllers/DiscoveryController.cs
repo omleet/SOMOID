f5a8df8907508acde0cc1e2542df3801fb7f7a13
Ruben Costa <rubenlavoscosta@gmail.com>
2025-12-03 17:56:08 +0000

    chore(refactor): Move SQL logic from DiscoveryController to a SQL helper file

--- SOMOID/Controllers/DiscoveryController.cs
+++ SOMOID/Controllers/DiscoveryController.cs
@@ -59,44 +59,12 @@ namespace SOMOID.Controllers
         )]
         public IHttpActionResult DiscoverContentInstances(string appName)
         {
-            var paths = new List<string>();
-            using (var conn = new SqlConnection(connection))
-            using (
-                var cmd = new SqlCommand(
-                    @"
-                SELECT a.[resource-name] AS appName,
-                       c.[resource-name] AS contName,
-                       ci.[resource-name] AS ciName
-                FROM [content-instance] ci
-                JOIN [container] c ON c.[resource-name] = ci.[container-resource-name]
-                JOIN [application] a ON a.[resource-name] = c.[application-resource-name]
-                WHERE a.[resource-name] = @appName
-                ORDER BY a.[resource-name], c.[resource-name], ci.[creation-datetime]",
-                    conn
-                )
-            )
+            try
             {
-                cmd.Parameters.AddWithValue("@appName", appName);
-
-                try
-                {
-                    conn.Open();
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            string aName = (string)reader["appName"];
-                            string cName = (string)reader["contName"];
-                            string ciName = (string)reader["ciName"];
-                            paths.Add($"/api/somiod/{aName}/{cName}/{ciName}");
-                        }
-                    }
-                    return Ok(paths);
-                }
-                catch (Exception ex)
-                {
-                    return InternalServerError(ex);
-                }
+                return Ok(SQLHelperInstance.GetAllContentInstances(appName));
+            }catch (Exception ex)
+            {
+                return InternalServerError(ex);
             }
         }
 
@@ -108,44 +76,12 @@ namespace SOMOID.Controllers
         )]
         public IHttpActionResult DiscoverSubscriptions(string appName, string containerName)
         {
-            var subscriptionPaths = new List<string>();
-            using (var conn = new SqlConnection(connection))
-            using (
-                var cmd = new SqlCommand(
-                    @"
-                SELECT s.[resource-name]
-                FROM [subscription] s
-                JOIN [container] c ON c.[resource-name] = s.[container-resource-name]
-                JOIN [application] a ON a.[resource-name] = c.[application-resource-name]
-                WHERE a.[resource-name] = @appName
-                  AND c.[resource-name] = @containerName
-                ORDER BY s.[creation-datetime]",
-                    conn
-                )
-            )
+            try
             {
-                cmd.Parameters.AddWithValue("@appName", appName);
-                cmd.Parameters.AddWithValue("@containerName", containerName);
-
-                try
-                {
-                    conn.Open();
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            string subName = (string)reader["resource-name"];
-                            subscriptionPaths.Add(
-                                $"/api/somiod/{appName}/{containerName}/subs/{subName}"
-                            );
-                        }
-                    }
-                    return Ok(subscriptionPaths);
-                }
-                catch (Exception ex)
-                {
-                    return InternalServerError(ex);
-                }
+                return Ok(SQLHelperInstance.GetAllSubscriptions(appName, containerName));
+            } catch(Exception ex)
+            {
+                return InternalServerError(ex);
             }
         }
 
--- SOMOID/Helpers/SQLHelper.cs
+++ SOMOID/Helpers/SQLHelper.cs
@@ -68,5 +68,81 @@ namespace SOMOID.Helpers
             }
             return containerPaths;
         }
+
+        public List<string> GetAllContentInstances(string appName)
+        {
+            var paths = new List<string>();
+            using (var conn = new SqlConnection(connection))
+            {
+                using (
+                    var cmd = new SqlCommand(
+                        @"
+                        SELECT a.[resource-name] AS appName,
+                               c.[resource-name] AS contName,
+                               ci.[resource-name] AS ciName
+                        FROM [content-instance] ci
+                        JOIN [container] c ON c.[resource-name] = ci.[container-resource-name]
+                        JOIN [application] a ON a.[resource-name] = c.[application-resource-name]
+                        WHERE a.[resource-name] = @appName
+                        ORDER BY a.[resource-name], c.[resource-name], ci.[creation-datetime]",
+                        conn
+                    )
+                )
+                {
+                    cmd.Parameters.AddWithValue("@appName", appName);
+
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string aName = (string)reader["appName"];
+                            string cName = (string)reader["contName"];
+                            string ciName = (string)reader["ciName"];
+                            paths.Add($"/api/somiod/{aName}/{cName}/{ciName}");
+                        }
+                    }
+                }
+            }
+            return paths;
+        }
+
+        public List<string> GetAllSubscriptions(string appName, string containerName)
+        {
+            var subscriptionPaths = new List<string>();
+            using (var conn = new SqlConnection(connection))
+            {
+                using (
+                    var cmd = new SqlCommand(
+                        @"
+                SELECT s.[resource-name]
+                FROM [subscription] s
+                JOIN [container] c ON c.[resource-name] = s.[container-resource-name]
+                JOIN [application] a ON a.[resource-name] = c.[application-resource-name]
+                WHERE a.[resource-name] = @appName
+                  AND c.[resource-name] = @containerName
+                ORDER BY s.[creation-datetime]",
+                        conn
+                    )
+                )
+                {
+                    cmd.Parameters.AddWithValue("@appName", appName);
+                    cmd.Parameters.AddWithValue("@containerName", containerName);
+
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string subName = (string)reader["resource-name"];
+                            subscriptionPaths.Add(
+                                $"/api/somiod/{appName}/{containerName}/subs/{subName}"
+                            );
+                        }
+                    }
+                }
+            }
+            return subscriptionPaths;
+        }
     }
 }
